6811f6540f379952f7bbc732452e2238403b701e
Moritz André Myrseth <moritzmyrz@gmail.com>
2023-12-05 11:15:21 +0100

    fix: retry on failed DAS-check

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ def is_domain_available(domain: str) -> bool:
     try:
         domain_info = whois.whois(domain)
         return False
-    except:
+    except Exception as e:
+        if "Connection reset by peer" in str(e):
+            print(f"Connection error for {domain}, retrying...")
+            time.sleep(5)
+            return is_domain_available(domain)
         return True
 
 
@@ -14,7 +18,7 @@ def check_domains_from_file(input_file_path: str, output_file_path: str) -> None
     with open(input_file_path, 'r') as infile, open(output_file_path, 'w') as outfile:
         for line in infile:
             word = line.strip()
-            if 5 > len(word) > 1:
+            if 6 > len(word) > 1:
                 domain_name = f"{word}.no"
                 if is_domain_available(domain_name):
                     print(f"Available: {domain_name}")
@@ -22,7 +26,7 @@ def check_domains_from_file(input_file_path: str, output_file_path: str) -> None
                 time.sleep(1)
 
 
-input_file = 'words_alpha.txt'
+input_file = 'clean_words_alpha.txt'
 output_file = 'available_domains.txt'
 
 check_domains_from_file(input_file, output_file)
